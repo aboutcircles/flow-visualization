350adfaa9ac5d10e3fe8ea273afbce8d8e458c14
hdser <hugo.serodio@gnosis.io>
2025-02-25 15:27:00 +0000

    update config

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: '/flow-visualization/pathfinder/',  
+  base: '/flow-visualization/',  
   plugins: [react()],
   resolve: {
     alias: {
